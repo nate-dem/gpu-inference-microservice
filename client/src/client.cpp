0fd536f51c32a2339efe698cabdcb33c1cb81041
nate-dem <natedem@stanford.edu>
2025-07-26 20:52:55 -0700

    set up initial version of the client

---
+++ client/src/client.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include "grpc_client.h"
+
+namespace tc = triton::client;
+
+int main(int argc, char** argv) {
+    const std::string server_url = "localhost:8001";
+    const std::string model_name = "custom_model";
+
+    std::unique_ptr<tc::InferenceServerGrpcClient> client;
+    tc::Error error = tc::InferenceServerGrpcClient::Create(&client, server_url);
+
+    if (!error.IsOk()) {
+        std::cerr << "Error with creating Trition client: " << error << std::endl;
+	return 1;
+    }
+
+    return 0;
+}
