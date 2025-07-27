d3ded292b98184d89707d57429475fa01774054e
nate-dem <natedem@stanford.edu>
2025-07-26 19:47:21 -0700

    wrote initial outline for backend.cpp - need to update after I've written the CUDA kernel

---
+++ backend/src/backend.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include "triton/core/tritonbackend.h"
+
+extern "C" void launch_kernel(const float* input, float* output, int size);
+extern "C" TRITONSERVER_Error*
+
+TRITONBACKEND_ModelInstanceExecute(TRITONBACKEND_ModelInstance* instance, TRITONBACKEND_Request** requests, const uint32_t request_count) {
+    for (uint32_t r = 0; r < request_count; ++r) {
+        TRITONBACKEND_Request* request = requests[r];
+        TRITONBACKEND_Response* response;
+	TRITONSERVER_Error* error = TRITONBACKEND_ResponseNew(&response, request);
+	
+	if (error != nullptr) {
+	   return error; 
+	}
+
+	TRITONBACKEND_ResponseSend(response, TRITONSERVER_RESPONSE_COMPLETE_FINAL, nullptr);
+	TRITONBACKEND_RequestRelease(request, TRITONSERVER_REQUEST_RELEASE_ALL);
+    }
+
+    return nullptr;
+}
+
+# lifecycle functions for Triton
+extern "C" TRITONSERVER_Error*
+TRITONBACKEND_Initialize(TRITONBACKEND_Backend* backend) {
+    std::cout << "Custom backend initialized." << std::endl;
+    return nullptr;
+}
+
+extern "C" TRITONSERVER_Error*
+TRITONBACKEND_Finalize(TRITONBACKEND_Backend* backend) {
+    std::cout << "Custom backend finalized." << std::endl;
+    return nullptr; 
+}
+
+extern "C" TRITONSERVER_Error*
+TRITONBACKEND_ModelInitialize(TRITONBACKEND_Model* model) {
+    std::cout << "Custom model initialized." << std::endl;
+    return nullptr; 
+}
+
+extern "C" TRITONSERVER_Error*
+TRITONBACKEND_ModelFinalize(TRITONBACKEND_Model* model) {
+    std::cout << "Custom model finalized." << std::endl;
+    return nullptr; 
+}
+
+extern "C" TRITONSERVER_Error*
+TRITONBACKEND_ModelInstanceInitialize(TRITONBACKEND_ModelInstance* instance) {
+    std::cout << "Custom model instance initialized." << std::endl;
+    return nullptr; 
+}
+
+extern "C" TRITONSERVER_Error*
+TRITONBACKEND_ModelInstanceFinalize(TRITONBACKEND_ModelInstance* instance) {
+    std::cout << "Custom model instance finalized." << std::endl;
+    return nullptr;
+}
+
+extern "C" TRITONSERVER_Error*
+TRITONBACKEND_ModelInstanceFinalize(TRITONBACKEND_ModelInstance* instance) {
+    std::cout << "Custom model instance finalized." << std::endl;
+    return nullptr;
+} 
