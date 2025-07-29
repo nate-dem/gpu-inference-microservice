4c4e53559f776b841354960d997fb7972ee0f44d
nate-dem <natedem@stanford.edu>
2025-07-28 22:17:40 -0700

    finished launchKernel implementation

--- backend/src/kernel.cu
+++ backend/src/kernel.cu
@@ -9,6 +9,24 @@ __global__ void kernel(const float* input, float* output, int size) {
 }
 
 # called in backend.cpp
-extern "C" void launch_kernel(const float* input, float* output, int size) {
-    std::cout << "Launching CUDA kernel..." << std::endl;
+extern "C" void launch_kernel(const float* input, float* output, size_t size) {
+    int blockSize = 256;
+    int gridSize = (size + blockSize - 1) / blockSize;    
+
+    float* deviceInput = nullptr;
+    float * deviceOutput = nullptr;
+    size_t bytes = size * sizeof(float);
+
+    cudaMalloc((void**)&deviceInput, bytes);
+    cudaMalloc((void**)&deviceOutput, bytes);
+
+    cudaMemcpy(deviceInput, input, bytes, cudaMemcpyHostToDevice);
+
+    kernel<<<gridSize,blockSize>>>(deviceInput, deviceOutput, size);
+
+    cudaMemcpy(output, deviceOutput, bytes, cudaMemcpyDeviceToHost);
+
+    cudaFree(deviceInput);
+    cudaFree(deviceOutput); 
+     
 }
