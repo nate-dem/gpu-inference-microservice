b1daf1eff202974eb9d3e560ba39e299c154c206
nate-dem <natedem@stanford.edu>
2025-07-30 20:10:55 -0700

    implemented client functionality

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -16,5 +16,44 @@ int main(int argc, char** argv) {
 	return 1;
     }
 
+    std::vector<int64_t> shape = {1, 16};
+    const std::string input_name = "INPUT0";
+    const std::string output_name = "OUTPUT0";
+    const std::string data_type = "FP32";
+    const float size = 16;
+
+    tc::InferInput* input_tensor;
+    tc::InferInput::Create(&input_tensor, input_name, shape, data_type);
+
+    std::vector<float> input_data(size);
+    for (size_t i = 0; i < size; ++i) {
+	input_data[i] = static_cast<float>(i);
+    }
+
+    input_tensor->AppendRaw(reinterpret_cast<const uint8_t*>(input_data.data()),
+                      input_data.size() * sizeof(float));
+
+    tc::InferRequestedOutput* output_tensor;
+    tc::InferRequestedOutput::Create(&output_tensor, output_name);
+
+	
+    tc::InferResult* result;
+    err = client->Infer(&result, {input_tensor}, {output_tensor});
+    if (!err.IsOk()) {
+	return 1;
+    }
+
+    std::vector<float> output_data;
+    err = result->RawData(output_name, (const uint8_t**)&output_data, &output_byte_size);
+    if (!err.IsOk()) {
+	return 1;
+    }
+
+    for (const auto& val : input_data) { std::cout << val << " "; }
+
+    for (size_t i = 0; i < output_byte_size / sizeof(float) ; ++i) {
+	std::cout << output_data[i] << " ";
+    }
+
     return 0;
 }
