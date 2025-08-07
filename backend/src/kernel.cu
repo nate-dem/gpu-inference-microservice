1e3900d31e490e83ad1b85cd40a31b1c26a8d331
nate-dem <natedem@stanford.edu>
2025-08-06 20:03:18 -0700

    simplified kernel for now to work with stream

--- backend/src/kernel.cu
+++ backend/src/kernel.cu
@@ -8,23 +8,9 @@ __global__ void kernel(const float* input, float* output, int size) {
     }
 }
 
-extern "C" void launch_kernel(const float* input, float* output, size_t size) {
-    int blockSize = 256;
-    int gridSize = (size + blockSize - 1) / blockSize;    
+extern "C" void launch_kernel(const float* input, float* output, size_t size, cudaStream_t stream) {
+    int threads = 256;
+    int blocks = (size + threads - 1) / threads;
 
-    float* d_in = nullptr;
-    float* d_out = nullptr;
-    size_t bytes = size * sizeof(float);
-
-    cudaMalloc((void**)&d_in, bytes);
-    cudaMalloc((void**)&d_out, bytes);
-
-    cudaMemcpy(d_in, input, bytes, cudaMemcpyHostToDevice);
-
-    kernel<<<gridSize,blockSize>>>(d_in, d_out, size);
-
-    cudaMemcpy(output, d_out, bytes, cudaMemcpyDeviceToHost);
-
-    cudaFree(d_in);
-    cudaFree(d_out);      
+    kernel<<<blocks, threads, 0, stream>>>(input, output, size);
 }
